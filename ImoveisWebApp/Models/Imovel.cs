5f2a1f9c017e74daafc8923d9cfbc696cce06785
Ivo Capanema <ivoliveirabh@hotmail.com>
2022-06-02 01:04:39 -0300

    Criado modelo para Imovel

---
+++ ImoveisWebApp/Models/Imovel.cs
@@ -0,0 +1,17 @@
+﻿namespace ImoveisWebApp.Models
+{
+    public class Imovel
+    {
+        public int Id { get; set; }
+        public int Tipo { get; set; }
+        public string ValorVenda { get; set; }
+        public string ValorLocacao { get; set; }
+        public string Endereco { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Cep { get; set; }
+    }
+}
