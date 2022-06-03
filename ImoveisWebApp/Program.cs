4b496045239fb72bfb6e9cdf347a0da766860040
Ivo Capanema <ivoliveirabh@hotmail.com>
2022-06-03 20:38:11 -0300

    Alterado controller padrão para imoveis

--- ImoveisWebApp/Program.cs
+++ ImoveisWebApp/Program.cs
@@ -27,6 +27,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Imoveis}/{action=Listar}/{id?}");
 
 app.Run();
