5994ca91ec22695db625fb90a4f86760df312098
Ivo Capanema <ivoliveirabh@hotmail.com>
2022-06-03 22:29:02 -0300

    Alterações nos links de referência das páginas

--- ImoveisWebApp/Models/ErrorViewModel.cs
+++ ImoveisWebApp/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace ImoveisWebApp.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
